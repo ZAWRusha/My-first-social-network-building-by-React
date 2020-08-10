d9ee580afef45cf589bb9d8db53c58885ad2fcc2
Malkov Dmitry <fly-agaric@bk.ru>
2020-08-10 15:16:47 +0700

    Created component Message

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import s from './Chat.module.css'
 import Contact from './Contact/Contact'
+import Message from './Message/Message'
 // import { NavLink } from 'react-router-dom'
 
 const Chat = (props) => {
@@ -10,19 +11,18 @@ const Chat = (props) => {
 
 				<div className={s.contacts_wrapper}>
 					<h4 className={s.title}>Contacts</h4>
-					<Contact id="1" niсkname="Dimych Kuzyberdin" message="Lorem ipsum dolor sit amet consectetur, adipisicing elit..." unread="5" />
-					<Contact id="2" niсkname="Petr Kharitonov" message="Although VS Code has some great built-in functionality for Git" unread="9" />
-					<Contact id="3" niсkname="Alla Polyanskaya" message="Lorem ipsum dolor sit amet consectetur, adipisicing elit adipisicing elit adipisicing elit" unread="1" />
-					<Contact id="4" niсkname="Svetlana Torgasheva" message="Who is there?" unread="" />
-					<Contact id="5" niсkname="Tony Bochka" message="Dolor sit amet consectetur, adipisicing elit..." unread="" />
+					<Contact id="1" niсkname="Dimych Kuzyberdin" unread="5" time="12:34" message="Lorem ipsum dolor sit amet consectetur, adipisicing elit..." />
+					<Contact id="2" niсkname="Petr Kharitonov" unread="9" time="12:34" message="Although VS Code has some great built-in functionality for Git" />
+					<Contact id="3" niсkname="Alla Polyanskaya" unread="1" time="12:35" message="Lorem ipsum dolor sit amet consectetur, adipisicing elit adipisicing elit adipisicing elit" />
+					<Contact id="4" niсkname="Svetlana Torgasheva" unread="" time="14:54" message="Who is there?" />
+					<Contact id="5" niсkname="Tony Bochka" unread="" time="16:01" message="Dolor sit amet consectetur, adipisicing elit..." />
 				</div>
 
 				<div className={s.messages}>
 					<h4 className={s.title}>Messages</h4>
-
-					<div className={s.message}>Hi</div>
-					<div className={s.message}>How is your study?</div>
-					<div className={s.message}>I'am ok</div>
+					<Message message="Hi" />
+					<Message message="How is your study?" />
+					<Message message="I'am ok" />
 				</div>
 			</div>
 
---
+++ src/components/Chat/Message/Message.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Message.module.css'
+// import { NavLink } from 'react-router-dom'
+
+const Message = (props) => {
+	return (
+
+		<div className={s.message}>{props.message}</div>
+	)
+}
+
+export default Message
